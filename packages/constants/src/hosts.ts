598293b2f4dff9b520851c0093316b991d8f63f7
Kevon <dev@pro.kevon.net>
2026-01-15 09:50:14 +0300

    refactor: update cross origins list

--- packages/constants/src/hosts.ts
+++ packages/constants/src/hosts.ts
@@ -8,6 +8,12 @@
 export const crossOrigins = [
   'localhost',
   '127.0.0.1',
-  'devokrann.vercel.app', // vercel deployment
   'domain.com', // production domain
+  'devokrann.vercel.app', // vercel deployment
+  'motown-pizza-server.vercel.app',
+  'motown-pizza-client-admin.vercel.app',
+  'motown-pizza-client-pos.vercel.app',
+  'motown-pizza-client-web.vercel.app',
+  'motown-pizza-client-kds.vercel.app',
+  'motown-pizza-server.vercel.app',
 ];
