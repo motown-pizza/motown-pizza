56922d06934b6d2cc6df29f77aaff59f462c36a6
Kevon <dev@pro.kevon.net>
2026-01-30 06:02:55 +0300

    feat: setup table management

--- apps/client-pos/src/components/common/cards/table.tsx
+++ apps/client-pos/src/components/common/cards/table.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
+  ActionIcon,
   Avatar,
   AvatarGroup,
   Card,
@@ -15,28 +16,79 @@ import { TableGet } from '@repo/types/models/table';
 import BadgeTableStatus from '../badges/table-status';
 import { useStoreTableBooking } from '@repo/libraries/zustand/stores/table-booking';
 import { useStoreOrder } from '@repo/libraries/zustand/stores/order';
+import { useOrderPlacementData } from '@/hooks/order';
+import { IconCheck } from '@tabler/icons-react';
+import { ICON_SIZE, ICON_STROKE_WIDTH } from '@repo/constants/sizes';
+import { TableBookingStatus } from '@repo/types/models/enums';
+import { useStoreOrderPlacement } from '@repo/libraries/zustand/stores/order-placement';
+import { useRouter } from 'next/navigation';
+import { useTableBookingActions } from '@repo/hooks/actions/table-booking';
+import { generateUUID } from '@repo/utilities/generators';
+import { useTableStatus } from '@/hooks/table';
+import { useOrderActions } from '@repo/hooks/actions/order';
 
 export default function Table({ props }: { props: TableGet }) {
+  const tableBookingIdRef = useRef(generateUUID());
+  const router = useRouter();
+
+  const { setOrderDetails } = useStoreOrderPlacement();
+  const { orderUpdate } = useOrderActions();
+  const { tableBookingCreate } = useTableBookingActions();
+  const { orderDetails } = useOrderPlacementData();
   const { tableBookings } = useStoreTableBooking();
   const { orders } = useStoreOrder();
 
-  const tableBookingIds = tableBookings
-    ?.filter((tb) => tb.table_id == props.id)
-    ?.map((tbc) => tbc.id);
+  const { isBooked, isOccupied } = useTableStatus({ table: props });
 
-  const ordersInBookings = orders?.filter(
-    (oi) =>
-      oi.table_booking_id && tableBookingIds?.includes(oi.table_booking_id)
-  );
+  const [customers, setCustomers] = useState<any[]>([]);
 
-  let customers: any[] = [];
+  useEffect(() => {
+    if (tableBookings === undefined) return;
+    if (tableBookings === null) return;
+    if (orders === undefined) return;
+    if (orders === null) return;
 
-  ordersInBookings?.map((oib) => {
-    customers.push({
-      name: oib.customer_name,
-      phone: oib.customer_phone,
+    const tableBookingIds = tableBookings
+      ?.filter((tb) => tb.table_id == props.id)
+      ?.map((tbc) => tbc.id);
+
+    const ordersInBookings = orders?.filter(
+      (oi) =>
+        oi.table_booking_id && tableBookingIds?.includes(oi.table_booking_id)
+    );
+
+    setCustomers(
+      ordersInBookings?.map((oib) => {
+        return { name: oib.customer_name, phone: oib.customer_phone };
+      })
+    );
+  }, [tableBookings, orders]);
+
+  const handleTableSelect = () => {
+    if (orderDetails === undefined) return;
+    if (orderDetails === null) return;
+
+    tableBookingCreate({
+      id: tableBookingIdRef.current,
+      number_of_persons: (orderDetails.guest_count || 0) + 1,
+      table_booking_status: TableBookingStatus.WAITING,
+      table_id: props.id,
     });
-  });
+
+    setOrderDetails({
+      ...orderDetails,
+      table_booking_id: tableBookingIdRef.current,
+    });
+
+    setTimeout(() => {
+      orderUpdate({
+        ...orderDetails,
+        table_booking_id: tableBookingIdRef.current,
+      });
+
+      router.push(`/pos/menu?orderId=${orderDetails.id}`);
+    }, 2000);
+  };
 
   return (
     <Card bg={'var(--mantine-color-dark-9)'}>
@@ -62,17 +114,23 @@ export default function Table({ props }: { props: TableGet }) {
             </Text>
           </Group>
 
-          <Group justify="end">
+          <Group justify="end" mih={38}>
             <AvatarGroup>
-              <Avatar key={'Jane Smith'} name={'Jane Smith'} color="initials" />
-              <Avatar key={'Mike Kane'} name={'Mike Kane'} color="initials" />
-              <Avatar
-                key={'Solomon Lane'}
-                name={'Solomon Lane'}
-                color="initials"
-              />
-              <Avatar>+5</Avatar>
+              {customers.map((c, i) => (
+                <Avatar key={i} name={c.name} color="initials" />
+              ))}
             </AvatarGroup>
+
+            {!!orderDetails?.customer_name && (
+              <ActionIcon
+                size={34.8}
+                // variant="light"
+                disabled={isOccupied || isBooked}
+                onClick={handleTableSelect}
+              >
+                <IconCheck size={ICON_SIZE} stroke={ICON_STROKE_WIDTH} />
+              </ActionIcon>
+            )}
           </Group>
         </Group>
       </Stack>
