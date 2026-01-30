8ead246812e0edc288103aedc0083bf731e92ef1
Kevon <dev@pro.kevon.net>
2026-01-30 10:39:37 +0300

    feat: setup order management

--- apps/client-admin/src/app/(dashboard)/dashboard/orders/page.tsx
+++ apps/client-admin/src/app/(dashboard)/dashboard/orders/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Metadata } from 'next';
 import LayoutPage from '@repo/components/layout/page';
+import PartialPageAdminDashboardOrders from '@/components/partial/page/dashboard/orders';
 
 export const metadata: Metadata = { title: 'Orders' };
 
 export default function Orders() {
   return (
     <LayoutPage>
-      <div>Orders</div>
+      <PartialPageAdminDashboardOrders />
     </LayoutPage>
   );
 }
