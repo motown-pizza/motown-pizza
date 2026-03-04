f267a31b8e75cdbea7a0bbf2d855cd563a515263
Kevon <dev@pro.kevon.net>
2026-03-04 12:03:02 +0300

    feat: setup back office user authentication

--- apps/client-admin/src/app/layout.tsx
+++ apps/client-admin/src/app/layout.tsx
@@ -23,6 +23,7 @@ import ProviderSync from '@/components/provider/sync';
 import { APP_NAME } from '@repo/constants/app';
 import { mantine } from '@/data/styles';
 import { DEFAULT_COLOR_SCHEME } from '@repo/constants/other';
+import { createClient } from '@repo/libraries/supabase/server';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -44,6 +45,9 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+  const { data: session } = await supabase.auth.getUser();
+
   return (
     <html
       lang="en"
@@ -63,7 +67,7 @@ export default async function RootLayout({
           appThemeProps={{ styleSheets: { ...mantine } }}
           colorScheme={DEFAULT_COLOR_SCHEME}
         >
-          <ProviderStore>
+          <ProviderStore props={{ sessionUser: session.user }}>
             <ProviderSync>{children}</ProviderSync>
           </ProviderStore>
         </ProviderMantine>
