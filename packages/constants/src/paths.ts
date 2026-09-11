0669ce4056a73715dac5dc85cf5cd16ccce5defb
kevon <dev@kevon.net>
2026-09-11 09:42:14 +0300

    feat: update shared Vercel substring for consistency across environments

--- packages/constants/src/paths.ts
+++ packages/constants/src/paths.ts
@@ -1,4 +1,4 @@
-export const SHARED_VERCEL_SUBSTRING = 'motown-pizza';
+export const SHARED_VERCEL_SUBSTRING = 'motown';
 const VERCEL_TEAM_SLUG = SHARED_VERCEL_SUBSTRING + '-team';
 const vercelEnv = process.env.NEXT_PUBLIC_VERCEL_ENV;
 const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
