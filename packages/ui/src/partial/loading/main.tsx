38201223eedefae8b2e9ea478a74b4401062053a
kevon <dev@kevon.net>
2026-09-11 16:13:20 +0300

    feat: enhance loading component with image display and improved layout

--- packages/ui/src/partial/loading/main.tsx
+++ packages/ui/src/partial/loading/main.tsx
@@ -1,12 +1,24 @@
 import { Center, Stack } from '@mantine/core';
 import { LayoutSection } from '../../layout/section';
 import { LoaderMain } from '../../loader/main';
+import { ImageDefault } from '../../image/default';
+import { APP_NAME, images } from '@repo/constants';
 
 export function PartialLoadingMain() {
   return (
     <LayoutSection id={'loading-main'}>
       <Center mih={'100vh'}>
-        <Stack align="center">
+        <Stack align="center" gap={'xl'}>
+          <ImageDefault
+            src={images.brand.logo.landscape.meta}
+            alt={APP_NAME.WEB}
+            height={{ base: 70 }}
+            width={{ base: 80 }}
+            fit="contain"
+            mode="grid"
+            style={{ transform: 'scale(1.3)' }}
+          />
+
           <LoaderMain />
         </Stack>
       </Center>
