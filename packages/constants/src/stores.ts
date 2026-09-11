bb3b3bf7861c388b711ba9c72f48765d25511cfd
kevon <dev@kevon.net>
2026-09-11 15:36:31 +0300

    fix: update iframe URLs for Lavington and Kileleshwa stores in stores.ts

--- packages/constants/src/stores.ts
+++ packages/constants/src/stores.ts
@@ -33,7 +33,7 @@ export const stores: StoreGet[] = [
     id: '2',
     title: 'Lavington Store',
     iframe:
-      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8236071102397!2d36.78705047501314!3d-1.2794373987083918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1736e655ec37%3A0xbeced74939d13fef!2sGs%20Arcade!5e0!3m2!1sen!2ske!4v1777834535514!5m2!1sen!2ske',
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.4029640529438!2d36.76820933850063!3d-1.2907574996742364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1a11b9fc68ad%3A0x6576c117231844b2!2sValley%20Arcade%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1789130043307!5m2!1sen!2ske',
     city: 'Nairobi',
     location: 'Valley Arcade/Lavington',
     phone: PHONES.PHONE2,
@@ -46,7 +46,7 @@ export const stores: StoreGet[] = [
     id: '3',
     title: 'Kileleshwa Store',
     iframe:
-      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.83874455918!2d36.79095937491689!3d-1.269664498718243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1744d1f496ef%3A0xa2a048835be09d59!2sRiverside%20Dr%2C%20Nairobi!5e0!3m2!1ssw!2ske!4v1764674963338!5m2!1ssw!2ske',
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8236071102397!2d36.78705047501314!3d-1.2794373987083918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1736e655ec37%3A0xbeced74939d13fef!2sGs%20Arcade!5e0!3m2!1sen!2ske!4v1777834535514!5m2!1sen!2ske',
     city: 'Nairobi',
     location: "G's Arcade, Kieni Road, Kileleshwa",
     phone: PHONES.PHONE3,
